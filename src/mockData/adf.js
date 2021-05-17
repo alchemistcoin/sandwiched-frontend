7811c372ca6cf6e2f326d0c10722c0cc5093096e
joeynelsonff <joenelsong@gmail.com>
2021-05-17 09:40:42 -0700

    test

--- src/mockData/adf.js
+++ src/mockData/adf.js
@@ -15,7 +15,7 @@
 
 
 
-// Lots of sandwiches
+//// Lots of sandwiches
 // ~/dev/sandwiched-backend  main !1 ?1  curl -N http://api.sandwiched.wtf/sandwiches/0xb1adceddb2941033a090dd166a462fe1c2029484
 // {
 //   "message": "Fetching transactions..."
