0c9eb77ae5e86507c51d4c8a38d1c58801202665
joenelsong <joenelsong@gmail.com>
2021-05-17 19:53:13 -0700

    add nvmrc back

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -5,7 +5,6 @@ import styled from "styled-components";
 const StyledHeader = styled.header`
   padding-top: 34px;
   padding-botom: 34px;
-  width: 100vw;
   height: 50px;
   border: 2px solid gold;
 `;
