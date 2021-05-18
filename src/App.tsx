3ac5b025f04fe634bad03056bdde68df88e27bb1
joenelsong <joenelsong@gmail.com>
2021-05-17 20:11:43 -0700

    hook up router

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,31 @@ function App() {
   return (
     <div className="App">
       <StyledHeader />
-      <LandingPage />
-
+      <Router>
+        <Switch>
+          <Route exact path="/" >
+            <LandingPage />
+          </Route>
+          <Route path="/:walletAddress">
+            <ResultsPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
+
+// TODO: Replace with real results page once implemented
+function ResultsPage() {
+    // We can use the `useParams` hook here to access
+    // the dynamic pieces of the URL.
+    // @ts-ignore
+  let { walletAddress } = useParams();
+    return (
+      <div>
+        <h3>Wallet Address: {walletAddress}</h3>
+      </div>
+    );
+  }
 
 export default App;
