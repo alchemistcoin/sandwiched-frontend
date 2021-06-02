afc26012ae814bd20a4a1fcb0ace293ef3085f79
joenelsong <joenelsong@gmail.com>
2021-06-01 22:51:17 -0700

    Hook up back-end Data Fetch for Results Page

--- src/components/SandwichPage/ResultsView.tsx
+++ src/components/SandwichPage/ResultsView.tsx
@@ -57,18 +57,18 @@ const ResultsView = () => (
       <BadSummarySandwichTable />
       <GoodSummarySandwichTable />
     </StyledSummarySandwichTableWrapper>
-    {/*<div style={{ maxWidth: '100%' }}>*/}
-    {/*  <MaterialTable*/}
-    {/*    columns={[*/}
-    {/*      { title: 'Adı', field: 'name' },*/}
-    {/*      { title: 'Soyadı', field: 'surname' },*/}
-    {/*      { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },*/}
-    {/*      { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } },*/}
-    {/*    ]}*/}
-    {/*    data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}*/}
-    {/*    title="Demo Title"*/}
-    {/*  />*/}
-    {/*</div>*/}
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        columns={[
+          { title: 'Adı', field: 'name' },
+          { title: 'Soyadı', field: 'surname' },
+          { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+          { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } },
+        ]}
+        data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
+        title="Demo Title"
+      />
+    </div>
   </StyledResultsView>
 )
 
--- src/components/SandwichPage/SandwichPage.tsx
+++ src/components/SandwichPage/SandwichPage.tsx
@@ -2,20 +2,45 @@ import React, { useState, useEffect } from 'react'
 import LoadingSandwiches from './LoadingSandwiches'
 import ResultsView from './ResultsView'
 import _isEmpty from 'lodash/isEmpty'
+import { useParams } from 'react-router-dom'
 
-const SandwichPage = () => {
+const SandwichPage = ({}) => {
   const [data, setData] = useState([{}])
+  // @ts-ignore
+  let { walletAddress } = useParams()
 
   // Fetch Sandwiches for wallet
   useEffect(() => {
-    setTimeout(() => {
-      setData((oldArray) => [...oldArray, { something: 'ok' }])
-    }, 3000)
-  })
+    // Create an scoped async function in the hook
+    const utf8Decoder = new TextDecoder('utf-8')
+    async function fetchStream(reader: any) {
+      for (;;) {
+        let { value: chunk, done: readerDone } = await reader.read()
+        if (readerDone) {
+          return
+        }
+        const message = utf8Decoder.decode(chunk)
+        const parsedMessage = JSON.parse(message)
+        // console.log(parsedMessage)
+        setData((oldArray) => [...oldArray, parsedMessage])
+      }
+    }
+    async function runFetchStream() {
+      // @ts-ignore
+      let reader = (await fetch(`https://api.sandwiched.wtf/sandwiches/${walletAddress}`)).body.getReader() // shouldn't hardcode this endpoint
+      fetchStream(reader)
+    }
+    // Execute the created function directly
+    runFetchStream()
+
+    // setTimeout(() => {
+    //   setData((oldArray) => [...oldArray, { something: 'ok' }])
+    // }, 3000)
+  }, [])
 
   console.log(data)
 
-  return <>{!_isEmpty(data[1]) ? <ResultsView /> : <LoadingSandwiches />}</>
+  return <div>{!_isEmpty(data[1]) ? <ResultsView /> : <LoadingSandwiches />}</div>
 }
 
 export default SandwichPage
