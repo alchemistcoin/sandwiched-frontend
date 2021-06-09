60184d72f0cc0fbb0ea23a9a91b2ea711a92c124
tom.heather <tom.heather@e2x.com>
2021-06-09 17:52:45 +0100

    header changes

--- src/components/SandwichPage/ResultsView.tsx
+++ src/components/SandwichPage/ResultsView.tsx
@@ -25,13 +25,42 @@ type DetailedTableProps = {
   fetchingComplete: boolean
 }
 
-const PageHeader = () => (
-  <StyledPageHeader>
-    <img src={sandwichPotion} />
-    <h1>Not too bad!</h1>
-    <p>these are the sandwiches we found</p>
-  </StyledPageHeader>
-)
+const PageHeader = (x: number) => {
+  let title: string
+  let body: React.ReactElement
+  if (x == 0) {
+    title = 'No sandwiches found.'
+    // body = 'Well played - use mistX.io to stay unsandwiched!'
+    body = (
+      <>
+        Well played - use{' '}
+        <a href="http://mistx.io" target="_blank" rel="noreferrer">
+          mistX.io
+        </a>{' '}
+        to stay unsandwiched!
+      </>
+    )
+  } else {
+    title = 'Uh-oh!'
+    body = (
+      <>
+        You&apos;ve been sandwiched, wtf were you thinking! Next time use{' '}
+        <a href="http://mistx.io" target="_blank" rel="noreferrer">
+          mistX.io
+        </a>
+      </>
+    )
+  }
+
+  return (
+    <StyledPageHeader>
+      <img src={sandwichPotion} />
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </StyledPageHeader>
+  )
+}
+
 /** Details Table Components */
 const AttributeItem = ({ mev }: { mev?: boolean }) => <>{mev && <StyledAttributesItem>MEV</StyledAttributesItem>}</>
 const EtherscanLink = ({ txId }: { txId: string }) => (
@@ -67,15 +96,20 @@ const ResultsView = ({ data = [], fetchingComplete }: DetailedTableProps) => {
   const detailedTableData = data.filter(filterSandwichesToDetailsTable).map(mapSandwichesToDetailsTable)
   // console.log('detailedTableData', detailedTableData)
 
+  const bestSandwichValue =
+    bestSandwich && bestSandwich.profit
+      ? `${Number(bestSandwich?.profit?.amount).toFixed()} ${bestSandwich?.profit?.currency}`
+      : 'None'
+
   return (
     <StyledResultsView>
-      <PageHeader />
+      {PageHeader(totalSandwiches)}
       <StyledSummarySandwichTableWrapper>
         <SummaryCard
           image={SummaryBestSandwich}
           backgroundColor={'#fdf0ca'}
           title={'juiciest'}
-          value={Number(bestSandwich?.profit?.amount).toFixed() + ` ${bestSandwich?.profit?.currency}` || '?'}
+          value={bestSandwichValue}
           valueColor={'#22da4a'}
         />
         <SummaryCard
