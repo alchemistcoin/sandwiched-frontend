3ba5befb04619bd4572080c3837ac23ae315ee9d
joenelsong <joenelsong@gmail.com>
2021-06-13 10:32:58 -0700

    remove debugging

--- src/components/SandwichPage/SandwichPage.tsx
+++ src/components/SandwichPage/SandwichPage.tsx
@@ -65,11 +65,8 @@ const SandwichPage = ({ onConnect, connected, walletAddress, resetApp }: Sandwic
     async function runFetchStream() {
       // @ts-ignore
       try {
-        //const response = await fetch(`http://localhost:3000/sandwiches/${walletAddress}`) // shouldn't hardcode this endpoint
         const response = await fetch(`https://api.sandwiched.wtf/sandwiches/${walletAddressFromUrl}`) // shouldn't hardcode this endpoint
-        console.log('response', response)
         const reader = ndjsonStream(response.body).getReader()
-        console.log('reader', reader)
         await fetchStream(reader)
       } catch (err) {
         console.error('runFetchStream() catch(err)', err) // ERR_SSL_PROTOCOL_ERROR
